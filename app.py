b9d4b31dc4a70c8d938a9b82662e5f5f4e3a81ba
mrifai07 <71766639+mrifai07@users.noreply.github.com>
2020-09-23 23:02:04 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+import matplotlib
+
+from helper import *
+
+matplotlib.use('Agg')
+
+app = Flask(__name__)
+
+data = load_data()
+
+@app.route("/")
+def index():
+	#generate value for cards
+	insurance = pd.read_csv('data/autoinsurance.csv')
+	percent_fraud = float(pd.crosstab(index=insurance['fraud_reported'],columns='count',normalize=True).loc['Y']*100)
+	# percent_fraud = float(pd.crosstab(index=________,columns='count',normalize=_____).loc['Y']*100)
+	fraud_loss = insurance.groupby('fraud_reported').sum()['total_claim_amount'].loc['Y']
+	# fraud_loss = ___.groupby(________).sum()['total_claim_amount'].loc['Y']
+	average_claim = insurance.total_claim_amount.median()
+	# average_claim = ___.________.median()
+	#compile card values as card_data
+	 
+	card_data = dict(
+		percent_fraud = f'{percent_fraud}%',
+		fraud_loss = f'US$ {fraud_loss:,}',
+		average_claim = f'US$ {average_claim:,}'
+	)
+
+	# generate plot
+	plot_age_res = plot_age(data)
+	plot_premium_res = plot_premium(data)
+	plot_incident_res = plot_incident(data)
+	plot_report_res = plot_report(data)
+	plot_baru1_res = plot_baru1(data)
+	plot_baru2_res = plot_baru2(data)
+
+	# render to html
+	return render_template('index.html',
+	card_data = card_data, 
+	plot_age_res=plot_age_res,
+	plot_premium_res=plot_premium_res,
+	plot_incident_res=plot_incident_res,
+	plot_report_res=plot_report_res,
+	plot_baru1_res = plot_baru1_res,
+	plot_baru2_res = plot_baru2_res
+		)
+
+
+if __name__ == "__main__": 
+    app.run(debug=True)
